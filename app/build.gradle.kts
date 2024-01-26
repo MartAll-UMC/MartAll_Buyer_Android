4413b3f087a56481bf68d588b2620af0675a714b
Jang Muyeong <136911104+wkdan@users.noreply.github.com>
2024-01-26 11:24:29 +0900

    feat/#2-HomeUI 전체메뉴 ~ 검색화면 UI 완성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-
     // Splash Screen
     implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
 
