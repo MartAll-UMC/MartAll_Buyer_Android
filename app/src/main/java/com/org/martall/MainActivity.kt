6580a61125be309672681499edf322dc1b94cfdb
민감자 <mouse0429@naver.com>
2024-02-04 02:35:32 +0900

    Fix: Conflict 오류 및 바텀네비게이션 에러 해결

--- app/src/main/java/com/org/martall/MainActivity.kt
+++ app/src/main/java/com/org/martall/MainActivity.kt
@@ -5,75 +5,53 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.org.martall.View.Likelist.DibsFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.org.martall.view.likelist.DibsFragment
 import com.org.martall.databinding.ActivityMainBinding
 import com.org.martall.view.home.HomeFragment
 import com.org.martall.view.mypage.customerservice.MyMartAllFragment
-import com.org.martall.view.nearme.NearMeFragment
-import com.org.martall.view.store.MartDetailInfoFragment
 import com.org.martall.view.store.LocalMartFragment
-import com.org.martall.view.store.LocalStoreFragment
-
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var fragmentHome = HomeFragment()
-    private var fragmentCategory = CategoryFragment()
-    private var fragmentNearMe = NearMeFragment()
-    private var fragmentMyMartAll = MyMartAllFragment()
-    private var fragmentHome = HomeFragment()
-    private var fragmentHeart = DibsFragment()
-    private val fragmentLocalStore = LocalStoreFragment()
+    private val fragmentHome = HomeFragment()
+    private val fragmentCategory = CategoryFragment()
+    private val fragmentMyMartAll = MyMartAllFragment()
+    private val fragmentHeart = DibsFragment()
     private val fragmentLocalMart = LocalMartFragment()
-    private var fragmentManager: FragmentManager = supportFragmentManager
-
-    // 상품 상세 확인
-    private var fragmentMartDetailInfo = MartDetailInfoFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
 
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initBottomNavigation()
-        showFragment(fragmentHome)
     }
 
         private fun initBottomNavigation() {
-            binding.bottomNavigationview.setOnItemSelectedListener { item ->
-                when (item.itemId) {
-                    R.id.menu_home -> {
-                        // 홈 화면으로 전환
-                        showFragment(fragmentHome)
-                        return@setOnItemSelectedListener true
-                    }
-                    R.id.menu_localMart -> transaction.replace(R.id.menu_frame_view, fragmentLocalStore)
-                        .commitAllowingStateLoss()
-                    R.id.menu_place -> {
-                        // 카테고리 화면으로 전환
-                        showFragment(fragmentCategory)
-                        return@setOnItemSelectedListener true
-                    }
-                    R.id.menu_heart -> transaction.replace(R.id.menu_frame_view, fragmentHeart)
-                        .commitAllowingStateLoss()
+            binding.bottomNavigationview.selectedItemId = R.id.menu_home
+            showFragment(fragmentHome)
 
-                    R.id.menu_user -> transaction.replace(R.id.menu_frame_view, fragmentMyMartAll)
-                        .commitAllowingStateLoss()
+            binding.bottomNavigationview.setOnItemSelectedListener {
+                val fragment = when (it.itemId) {
+                    R.id.menu_home -> fragmentHome
+                    R.id.menu_localMart -> fragmentLocalMart
+                    R.id.menu_place -> fragmentCategory
+                    R.id.menu_heart -> fragmentHeart
+                    R.id.menu_user -> fragmentMyMartAll
+                    else -> fragmentHome
                 }
-                false
+                showFragment(fragment)
+                true
             }
         }
 
         private fun showFragment(fragment: Fragment) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.menu_frame_view, fragment)
-                .commitAllowingStateLoss()
+                .addToBackStack(null)
+                .commit()
         }
     }
