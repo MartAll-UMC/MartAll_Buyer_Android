0fd178b27484cfde0c49dcb8100f80111ec22adc
민감자 <mouse0429@naver.com>
2024-02-06 13:43:46 +0900

    Style: 코드 formatting

--- app/src/main/java/com/org/martall/view/store/LocalMartFragment.kt
+++ app/src/main/java/com/org/martall/view/store/LocalMartFragment.kt
@@ -2,21 +2,21 @@ package com.org.martall.view.store
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.org.martall.adapter.LocalViewPagerAdapter
 import com.org.martall.databinding.FragmentLocalMartBinding
 
 class LocalMartFragment : Fragment() {
-    private lateinit var binding : FragmentLocalMartBinding
+    private lateinit var binding: FragmentLocalMartBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         binding = FragmentLocalMartBinding.inflate(inflater, container, false)
 
