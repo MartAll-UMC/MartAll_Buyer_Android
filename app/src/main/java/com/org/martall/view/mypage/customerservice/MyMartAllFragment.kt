de08d6e9494c02c476f2323ed616ee8e5bb825c8
민감자 <mouse0429@naver.com>
2024-02-06 13:42:18 +0900

    Fix: 내 정보 화면 UI 오류 수정

--- app/src/main/java/com/org/martall/view/mypage/customerservice/MyMartAllFragment.kt
+++ app/src/main/java/com/org/martall/view/mypage/customerservice/MyMartAllFragment.kt
@@ -3,31 +3,34 @@ package com.org.martall.view.mypage.customerservice
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.org.martall.view.likelist.DibsActivity
+import androidx.fragment.app.Fragment
+import com.org.martall.MainActivity
+import com.org.martall.R
+import com.org.martall.databinding.ActivityMainBinding
+import com.org.martall.databinding.FragmentDibsMartBinding
 import com.org.martall.databinding.FragmentMyMartAllBinding
 
 class MyMartAllFragment : Fragment() {
-    private lateinit var binding : FragmentMyMartAllBinding
+    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var binding: FragmentMyMartAllBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         binding = FragmentMyMartAllBinding.inflate(inflater, container, false)
+        mainBinding = (requireActivity() as MainActivity).binding
 
-        binding.likeGoodsBtn.setOnClickListener {
-            val intent = Intent(requireContext(),DibsActivity::class.java)
-            startActivity(intent)
+        binding.likeProductBtn.setOnClickListener {
+            mainBinding.bottomNavigationview.selectedItemId = R.id.menu_heart
         }
 
-        binding.likeGoodsBtn.setOnClickListener{
-            val intent = Intent(requireActivity(), DibsActivity::class.java)
-            Log.d("intent", "넘어감")
-            startActivity(intent)
+        binding.likeMartBtn.setOnClickListener {
+            mainBinding.bottomNavigationview.selectedItemId = R.id.menu_heart
+
         }
 
         binding.privacyPolicyPannel.setOnClickListener {
