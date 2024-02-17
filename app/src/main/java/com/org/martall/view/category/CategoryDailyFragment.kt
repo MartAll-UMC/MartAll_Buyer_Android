86728f60a4fe4f37ca54510cf845fa653408d0a6
Jang Muyeong <136911104+wkdan@users.noreply.github.com>
2024-02-17 22:14:34 +0900

    feat/#30 category API 적용 (목서버만)

--- app/src/main/java/com/org/martall/view/category/CategoryDailyFragment.kt
+++ app/src/main/java/com/org/martall/view/category/CategoryDailyFragment.kt
@@ -1,30 +1,76 @@
 package com.org.martall.view.category
 
+import CategoryRVAdapter
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.org.martall.adapter.CategoryRVAdapter
+import androidx.recyclerview.widget.GridLayoutManager
+import com.org.martall.BuildConfig
 import com.org.martall.databinding.FragmentCategoryDailyBinding
-import com.org.martall.model.dummyDailyItems
+import com.org.martall.interfaces.CategoryService
+import com.org.martall.interfaces.MartItemdibs
+import com.org.martall.model.SecondResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class CategoryDailyFragment : Fragment() {
 
     private lateinit var binding: FragmentCategoryDailyBinding
+
+    companion object {
+        fun newInstance(): CategoryDailyFragment {
+            return CategoryDailyFragment()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCategoryDailyBinding.inflate(inflater, container, false)
-       /* binding.rvProductList.adapter = CategoryRVAdapter(dummyDailyItems)*/
-        return binding.root
-    }
 
-    companion object {
-        fun newInstance(): CategoryDailyFragment {
-            return CategoryDailyFragment()
-        }
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.MOCK_CART_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val categoryService = retrofit.create(CategoryService::class.java)
+        val categoryCall = categoryService.getCategoryItem("생활용품", 0, 1000, "기본")
+        val martItemdibs = retrofit.create(MartItemdibs::class.java)
+
+        Log.d("Retrofit", "Making server request...")
+
+        categoryCall.enqueue(object : Callback<SecondResponse> {
+            override fun onResponse(
+                call: Call<SecondResponse>,
+                response: Response<SecondResponse>
+            ) {
+                if (response.isSuccessful) {
+                    Log.d("Retrofit", "Server request successful")
+                    val categoryResponse = response.body()
+                    categoryResponse?.result?.let { categoryItems ->
+                        val adapter = CategoryRVAdapter(categoryItems.item ?: listOf(), martItemdibs)
+                        binding.rvProductList.adapter = adapter
+                        binding.rvProductList.layoutManager = GridLayoutManager(requireContext(), 2)
+                    }
+                } else {
+                    Log.e("Retrofit", "Server request failed with code ${response.code()}")
+                    // Handle error response
+                }
+            }
+
+            override fun onFailure(call: Call<SecondResponse>, t: Throwable) {
+                Log.e("Retrofit", "Server request failed", t)
+                // Handle failure
+            }
+        })
+
+        return binding.root
     }
 }
