45217f906e7c5464797f36895c7ca968b47eafab
dusskdus <skdusb0007@naver.com>
2024-02-18 05:11:03 +0900

    feat/#34 : 알림 - 주문현황

---
+++ app/src/main/java/com/org/martall/model/NoticeOrderDTO.kt
@@ -0,0 +1,39 @@
+package com.org.martall.Model
+
+data class NoticeOrderDTO(
+    val timeStamp: String,
+    val status: Int,
+    val message: String,
+    val result: MartResult,
+    val success: Boolean
+){
+    data class MartResult(
+        val mart: Mart,
+        val cartItems: List<CartItem>
+    ){
+        data class Mart(
+            val martShopId: Int,
+            val martName: String,
+            val martCategory: List<MartCategory>,
+            val bookmarkCount: Int,
+            val likeCount: Int
+        ){
+            data class MartCategory(
+                val categoryName: String,
+                val index: String
+            )
+        }
+
+        data class CartItem(
+            val cartItemId: Int,
+            val itemId: Int,
+            val categoryName: String,
+            val picName: String,
+            val count: Int,
+            val itemName: String,
+            val price: Int
+        )
+
+    }
+
+}
