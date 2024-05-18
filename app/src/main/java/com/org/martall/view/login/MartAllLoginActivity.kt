94e06c82ef03010518943353c36cb43b720e7517
정다교 <hyunn0121@duksung.ac.kr>
2024-05-18 14:03:51 +0900

    feat: 마트올 자체 회원가입, 로그인 UI 수정 완료

--- app/src/main/java/com/org/martall/view/login/MartAllLoginActivity.kt
+++ app/src/main/java/com/org/martall/view/login/MartAllLoginActivity.kt
@@ -1,12 +1,16 @@
 package com.org.martall.view.login
 
 import androidx.appcompat.app.AppCompatActivity
+import com.org.martall.databinding.ActivityMartAllLoginBinding
 import android.os.Bundle
 import com.org.martall.R
+import com.org.martall.databinding.ActivitySignUpBinding
 
 class MartAllLoginActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMartAllLoginBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mart_all_login)
+        setContentView(binding.root)
     }
 }
